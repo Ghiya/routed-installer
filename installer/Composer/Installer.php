3f3392bdc5f95bf1c41f9ea5d0cbee5a67ee2d3e
Ghiya <ghiya.mikadze@gmail.com>
2018-03-21 14:43:08 +0300

    добавлена обработка параметров `extra`

--- installer/Composer/Installer.php
+++ installer/Composer/Installer.php
@@ -5,15 +5,81 @@ namespace extInstaller\Composer;
 use Composer\Package\PackageInterface;
 use Composer\Installer\LibraryInstaller;
 
+/**
+ * Class Installer
+ *
+ * @package extInstaller\Composer
+ * @author  Ghiya <ghiya@mikadze.me>
+ */
 class Installer extends LibraryInstaller
 {
+
+
+    /**
+     * Путь для установки пакета если не переопределён соответствующим параметром `extra`.
+     */
+    const INSTALL_ROOT_DEFAULT = 'extension';
+
+
+    /**
+     * Значение ключа параметра `extra` определяющего путь для установки пакета.
+     */
+    const INSTALL_ROOT_OPTION = 'install-path';
+
+
+    /**
+     * Значение ключа параметра `extra` определяющего производителя плагина для фильтрации в пути установки.
+     */
+    const VENDOR_OPTION = 'vendor';
+
+
+    /**
+     * Возвращает путь корневого каталога для установки пакета в зависимости от соответствующего `extra` параметра.
+     *
+     * @param PackageInterface $package
+     *
+     * @return string
+     */
+    protected function getPackageInstallRoot(PackageInterface $package)
+    {
+        $extraData = $package->getExtra();
+        return
+            !empty($extraData[self::INSTALL_ROOT_OPTION]) ?
+                $extraData[self::INSTALL_ROOT_OPTION] : self::INSTALL_ROOT_DEFAULT;
+    }
+
+
+    /**
+     * Возвращает фильтрованную часть пути от корневого каталога содержащую название пакета в зависимости от
+     * соответствующего `extra` параметра.
+     *
+     * @param PackageInterface $package
+     *
+     * @return bool|string
+     */
+    protected function getPackageSuffix(PackageInterface $package)
+    {
+        $extraData = $package->getExtra();
+        return
+            !empty($extraData[self::VENDOR_OPTION]) ?
+                preg_replace(
+                    "/" . $extraData[self::VENDOR_OPTION] . "\//i",
+                    "",
+                    $package->getPrettyName()
+                ) :
+                $package->getPrettyName();
+
+    }
+
+
     /**
      * {@inheritDoc}
      */
     public function getInstallPath(PackageInterface $package)
     {
-        return 'extended/' . substr($package->getPrettyName(), 7);
+        return $this->getPackageInstallRoot($package) . $this->getPackageSuffix($package);
     }
+
 
     /**
      * {@inheritDoc}
--- installer/Composer/Plugin.php
+++ installer/Composer/Plugin.php
@@ -6,9 +6,15 @@ use Composer\Composer;
 use Composer\IO\IOInterface;
 use Composer\Plugin\PluginInterface;
 
-
+/**
+ * Class Plugin
+ *
+ * @package extInstaller\Composer
+ * @author  Ghiya <ghiya@mikadze.me>
+ */
 class Plugin implements PluginInterface
 {
+
 
     public function activate(Composer $composer, IOInterface $io)
     {
